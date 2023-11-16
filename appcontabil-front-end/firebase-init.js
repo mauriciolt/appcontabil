7e040901b08a5b6b9b4986430271d1240274a57f
Mauricio Teixeira <mauriciolt@gmail.com>
2023-11-16 01:05:57 -0300

    Autenticação + Registro de novo usuário

---
+++ appcontabil-front-end/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMzYH8nvYTFN9ifwklznkGUUV9yLV2EY8",
+    authDomain: "appcontabil-49344.firebaseapp.com",
+    projectId: "appcontabil-49344",
+    storageBucket: "appcontabil-49344.appspot.com",
+    messagingSenderId: "1092289550669",
+    appId: "1:1092289550669:web:4b671dde9e7796c4e47f04"
+  };
+firebase.initializeApp(firebaseConfig);
